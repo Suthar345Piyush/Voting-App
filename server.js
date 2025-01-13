2114fd4a4d07eb833106a55a679c4a2bd08ddc71
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-01-13 23:41:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT , () => {
+  console.log("Server started at PORT 3000");
+});
+
